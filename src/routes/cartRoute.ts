3b471ebc0736cff5290780060179e9bcaa2e3aad
MohammadHishamm <mohammad2109652@miuegypt.edu.eg>
2025-08-29 19:04:53 +0300

    add item to cart /post endpoint

--- src/routes/cartRoute.ts
+++ src/routes/cartRoute.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { getActiveCartForUser } from "../services/cartService";
+import { getActiveCartForUser, addItemToCart } from "../services/cartService";
 import validateJWT, {ExtendedRequest} from "../middlewares/validateJWT";
 
 const router = express.Router();
@@ -11,3 +11,11 @@ router.get("/", validateJWT, async (req: ExtendedRequest, res) => {
      res.status(200).send(cart);
 })
 export default router;
+
+router.post("/items",validateJWT, async (req: ExtendedRequest, res) => {
+
+     const userId = req.user._id;
+     const {productId, quantity} = req.body;
+     const response = await addItemToCart({productId,userId, quantity});
+     res.status(200).send(response);
+})
--- src/services/cartService.ts
+++ src/services/cartService.ts
@@ -1,26 +1,68 @@
+import { parse } from "qs";
 import { cartModel } from "../modules/cartModel";
+import { productModel } from "../modules/productModel";
 
-interface CreateCartForUSer{
-    userId: string;
+interface CreateCartForUSer {
+  userId: string;
 }
 
+const createCartForUser = async ({ userId }: CreateCartForUSer) => {
+  const cart = await cartModel.create({ userId });
+  await cart.save();
+  return cart;
+};
 
-const createCartForUser= async ({userId}: CreateCartForUSer) =>{
-     const cart = await cartModel.create({userId})
-     await cart.save();
-     return cart;
+interface getActiveCartForUser {
+  userId: string;
 }
 
+export const getActiveCartForUser = async ({
+  userId,
+}: getActiveCartForUser) => {
+  let cart = await cartModel.findOne({ userId, status: "active" });
+  if (!cart) {
+    cart = await createCartForUser({ userId });
+  }
+  return cart;
+};
 
-interface getActiveCartForUser{
-    userId: string;
+interface addItemToCart {
+  productId: any;
+  userId: string;
+  quantity: string;
 }
 
-export const getActiveCartForUser = async ({userId}: getActiveCartForUser) => {
- let cart = await cartModel.findOne({userId, status: "active"});
- if(!cart){
-     cart = await createCartForUser({userId});
- }
- return cart;
-}
+export const addItemToCart = async ({
+  productId,
+  userId,
+  quantity,
+}: addItemToCart) => {
+  const cart = await getActiveCartForUser({ userId });
 
+  const existInCart = cart.items.find(
+    (p) => p.product.toString() === productId.toString()
+  );
+
+  if (existInCart) {
+    return { data: "item exists", statusCode: 400 };
+  }
+
+  const product = await productModel.findById(productId);
+  if (!product) {
+    return { data: "Product not found", statusCode: 400 };
+  }
+
+  if (product.stock < parseInt(quantity)) {
+    return { data: "Insufficient stock", statusCode: 400 };
+  }
+
+  cart.items.push({
+    product: productId,
+    unitPrice: product.price,
+    quantity: parseInt(quantity),
+  });
+   cart.totalAmount += product.price * parseInt(quantity);
+
+  const addedItems = await cart.save();
+  return { data: addedItems, statusCode: 200 };
+};
