99b4393a9b4938803cab6a924f8d205d3239a340
MohammadHishamm <mohammad2109652@miuegypt.edu.eg>
2025-08-30 16:48:29 +0300

    MUI installation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { HomePage } from './pages/home'
 import { LoginPage } from './pages/login'
+import Navbar from './components/navbar'
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar />
         <Routes>  
           <Route path="/login" element={<LoginPage />} />
           <Route path="/" element={<HomePage />} />
--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
 
+
+
 export const HomePage = () => {
   return (
-    <div>home</div>
+    <>
+      
+    </>
   )
 }
